fe195e9eedbfbafad3cf28eba7f968b13123523d
nickzaro <nickzaro@gmail.com>
2019-05-06 00:31:14 -0300

    agregado envio y recepcion de mensajes a todos los usuarios

--- index.js
+++ index.js
@@ -5,25 +5,26 @@ const path = require('path');
 const socketIO = require('socket.io');
 
 // settings-[por si viene setteado de otro lugar, sino 7777]
-app.set('port', process.env.PORT|| 7777);
+app.set('port', process.env.PORT || 7777);
 
 //static files
-app.use(express.static(path.join(__dirname,'/public/')));
+app.use(express.static(path.join(__dirname, '/public/')));
 
 //start the server
-const server = app.listen(app.get('port'),()=>{
-    console.log('server on port',app.get('port'));
+const server = app.listen(app.get('port'), () => {
+    console.log('server on port', app.get('port'));
 })
 
 // io contiene al socket listo
 const io = socketIO.listen(server);
 
 //hilo y socket para atender clientes
-io.on('connection',(socket)=>{
+io.on('connection', (socket) => {
     console.log('new connection: ', socket.id);
-    socket.on('chat-message',(data)=>{
+    socket.on('chat-message', (data) => {
         console.log(data);
-        
+        //emitir a todos los clientes conectados
+        io.sockets.emit('chat-message', data);
     })
 
 })
--- public/chatsimple.js
+++ public/chatsimple.js
@@ -3,17 +3,22 @@ const socket = io();
 
 // elementos del DOM
 let message = document.getElementById('message');
-let username=document.getElementById('username');
+let username = document.getElementById('username');
 let send = document.getElementById('send');
-let output =document.getElementById('output');
+let output = document.getElementById('output');
 let actions = document.getElementById('actions');
 
 //agregando el evento para el boton
-send.addEventListener('click',()=>{
+send.addEventListener('click', () => {
     console.log('que paso');
-    socket.emit('chat-message',
-    {
-        'username':username.value,
-        'message':message.value
+    socket.emit('chat-message', {
+        'username': username.value,
+        'message': message.value
     })
-})
+});
+
+socket.on('chat-message', (data) => {
+    actions.innerHTML += `<p>
+    <strong> ${data.username}
+    </strong>: ${data.message} </p> `
+});
