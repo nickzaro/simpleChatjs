c965bcd70bb3bfd1e767393a57bf8879d7846c0a
nickzaro <nickzaro@gmail.com>
2019-05-05 23:16:43 -0300

    simple server andando

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hi world');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const socketIO = require('socket.io');
+
+// settings-[por si viene setteado de otro lugar, sino 7777]
+app.set('port', process.env.PORT|| 7777);
+
+//static files
+app.use(express.static(path.join(__dirname,'/public/')));
+
+//start the server
+const server = app.listen(app.get('port'),()=>{
+    console.log('server on port',app.get('port'));
+})
+
+// io contiene al socket listo
+const io = socketIO.listen(server);
